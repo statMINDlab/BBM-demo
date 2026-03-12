4090b03a321495f8b1c1e4531b9c2e3afbe81011
diego <diegoderman@gmail.com>
2026-03-12 14:52:17 -0400

    FIX MERGE CONFLICT

--- src/0_setup.R
+++ src/0_setup.R
@@ -27,9 +27,6 @@ wb_path <- "/Applications/wb_view.app/Contents/usr/bin"
 ciftiTools.setOption("wb_path", wb_path) 
 
 # Set up paths
-#dir_HCP <- "/N/project/hcp_dcwan" # Location of HCP data
-<<<<<<< HEAD
-#dir_HCP <- "~/Documents/hcp_dcwan"
 #########################################################################
 #
 # INSERT YOUR OWN PATH TO THE HCP DATASET IN THE LINE BELOW TO RUN PIPELINE
@@ -42,16 +39,9 @@ dir_project <- "~/Documents/GitHub/BayesianBrainMapping-priors" # Path to GitHub
 
 dir_data <- file.path(dir_project, "data_OSF") # Path to data folder
 
-=======
-dir_HCP <- "~/Documents/hcp_dcwan"
-dir_project <- "~/Documents/GitHub/BBM-priors" # Path to GitHub folder
-
-dir_data <- file.path(dir_project, "data_OSF") # Path to data folder
-
 # Bring your own HCP access for both restricted and unrestricted data
 dir_HCP_demo <- "~/Documents/GitHub/BBM-priors/data_OSF/inputs/HCP_demo" # Path to folder with HCP demographics CSVs
 
->>>>>>> 52c59c9 (add brain map and engagement 1 session)
 # HCP_unrestricted_fname <- file.path(dir_data, "inputs", "unrestricted_HCP_demographics.csv")
 HCP_restricted_fname <- file.path(dir_HCP, "restricted_HCP.csv")
 # TEST PURPOSES ONLY TRYING WITH RESTRICTED DEMEOGRAPHICS
